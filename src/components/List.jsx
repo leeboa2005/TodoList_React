68b097052391f1c6e0ecf7766a357c5cb7b55419
leeboa2005 <leeboa2005@naver.com>
2024-09-23 23:50:35 +0900

    fix: 리스트 컴포넌트 리팩토링- TodoList로 코드 재사용성 개선

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,10 +2,28 @@ import React from 'react';
 import '../assets/style/list.css';
 import Todo from './Todo';
 
+const TodoList = ({ todos, setTodos, isDone, onCompleteHandler, onDeleteHandler }) => {
+    return (
+        <ul className="list_wrap">
+            {todos
+                .filter((todo) => todo.isDone === isDone)
+                .map((todo) => (
+                    <Todo
+                        todo={todo}
+                        key={todo.id}
+                        setTodos={setTodos}
+                        onDeleteHandler={onDeleteHandler}
+                        onCompleteHandler={onCompleteHandler}
+                    />
+                ))}
+        </ul>
+    );
+};
+
 const List = ({ todos, setTodos }) => {
     // 다른 형제 컴포넌트들의 event에 의해 부모컴포넌트에서 수정된 props들을 넘겨 받는다.
 
-    //todos 배열에서 todo.id가 피라미터와(selectedId) 일치하지 않는 요소만 추출해서 새로운 배열을 만듦.
+    // todos 배열에서 todo.id가 피라미터와(selectedId) 일치하지 않는 요소만 추출해서 새로운 배열을 만듦.
     const onDeleteHandler = (selectedId) => {
         const remainedTodos = todos.filter((todo) => {
             return todo.id !== selectedId;
@@ -13,7 +31,7 @@ const List = ({ todos, setTodos }) => {
         setTodos(remainedTodos);
     };
 
-    //완료 됬을때 누르면 isDone이 false -> true로 변경되면서 Done
+    // 완료 됬을때 누르면 isDone이 false -> true로 변경되면서 Done
     const onCompleteHandler = (selectedId) => {
         const newTodos = todos.map((todo) => {
             if (todo.id === selectedId) {
@@ -28,40 +46,23 @@ const List = ({ todos, setTodos }) => {
     return (
         <div className="list_container">
             <h3>KeepGoing</h3>
-            <ul className="list_wrap">
-                {todos.map((todo) => {
-                    // todo 완료 전
-                    if (todo.isDone === false) {
-                        return (
-                            <Todo
-                                todo={todo}
-                                key={todo.id}
-                                setTodos={setTodos}
-                                onDeleteHandler={onDeleteHandler}
-                                onCompleteHandler={onCompleteHandler}
-                            />
-                        );
-                    }
-                })}
-            </ul>
+            <TodoList
+                todos={todos}
+                setTodos={setTodos}
+                isDone={false}
+                onCompleteHandler={onCompleteHandler}
+                onDeleteHandler={onDeleteHandler}
+            />
             <h3>Done</h3>
-            <ul className="list_wrap done">
-                {todos.map((todo) => {
-                    // todo 완료 후
-                    if (todo.isDone === true) {
-                        return (
-                            <Todo
-                                todo={todo}
-                                key={todo.id}
-                                setTodos={setTodos}
-                                onDeleteHandler={onDeleteHandler}
-                                onCompleteHandler={onCompleteHandler}
-                            />
-                        );
-                    }
-                })}
-            </ul>
+            <TodoList
+                todos={todos}
+                setTodos={setTodos}
+                isDone={true}
+                onCompleteHandler={onCompleteHandler}
+                onDeleteHandler={onDeleteHandler}
+            />
         </div>
     );
 };
+
 export default List;
